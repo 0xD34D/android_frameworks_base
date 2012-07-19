43aabddcf0f8f36b64cfe74f0b215e20e51ae88a
0xD34D <clark@scheffsblend.com>
2012-07-18 17:37:46 -0700

    Added mass storage notifcation to TabletStatusBar
    Change-Id: Ie884ed4768fe323a3df625c445bdce94cd787f5f

--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
@@ -42,6 +42,7 @@ import android.os.IBinder;
 import android.os.Message;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.storage.StorageManager;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Slog;
@@ -142,6 +143,9 @@ public class TabletStatusBar extends BaseStatusBar implements
     NotificationData.Entry mNotificationDNDDummyEntry;
 
     HeightReceiver mHeightReceiver;
+
+    // storage
+    private StorageManager mStorageManager;
 
     ImageView mBackButton;
     View mHomeButton;
@@ -277,6 +281,11 @@ public class TabletStatusBar extends BaseStatusBar implements
         // Bt
         mBluetoothController.addIconView(
                 (ImageView)mNotificationPanel.findViewById(R.id.bluetooth));
+
+        // storage
+        mStorageManager = (StorageManager) context.getSystemService(Context.STORAGE_SERVICE);
+        mStorageManager.registerListener(
+                new com.android.systemui.usb.StorageNotification(context));
 
         // network icons: either a combo icon that switches between mobile and data, or distinct
         // mobile and data icons
